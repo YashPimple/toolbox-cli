8e439ef649bb2e17d80f3c17068e231793f9c266
Yash Pimple <97302447+YashPimple@users.noreply.github.com>
2023-09-10 20:53:51 +0530

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -4,18 +4,23 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/YashPimple/Toolbox/cmd/cluster"
 	"github.com/YashPimple/Toolbox/cmd/info"
 	"github.com/YashPimple/Toolbox/cmd/net"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "Toolbox",
-	Short: "",
-	Long: `Toolbox is a command-line interface (CLI) tool written in Go. It leverages the power of Cobra, a CLI library for Go, to offer a seamless user experience.`,
+	Short: "A command-line interface (CLI) tool written in Go",
+	Long:  ``,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -30,19 +35,46 @@ func Execute() {
 	}
 }
 
-func addSubCommandPalette() {
-	rootCmd.AddCommand(net.NetCmd)
-	rootCmd.AddCommand(info.InfoCmd)
-}
 func init() {
+	cobra.OnInitialize(initConfig)
+
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.Toolbox.yaml)")
+	//add my sub-command palatte
+	rootCmd.AddCommand(info.InfoCmd)
+	rootCmd.AddCommand(net.NetCmd)
+	rootCmd.AddCommand(cluster.Delete)
+	rootCmd.AddCommand(cluster.CmdCreate)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.Toolbox.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	addSubCommandPalette()
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".Toolbox" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".Toolbox")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
 }
